fb8a8adb35da70df39d8346a4b69479c60d7cb19
Justus Dieckmann <justusdieckmann@wwu.de>
2020-04-28 12:11:46 +0200

    Testscript: Require login

--- test.php
+++ test.php
@@ -7,6 +7,8 @@ require_once(__DIR__ . '/../../config.php');
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url(new moodle_url('/local/chunkupload/test.php'));
 
+require_login();
+
 echo $OUTPUT->header();
 $mform = new testmform(new moodle_url('/local/chunkupload/test.php'));
 var_dump($mform->get_data());
