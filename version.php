21b8f828a61235d938df7d047c8280d8c913c1ff
Thomas Niedermaier <thomas.niedermaier@gmail.com>
2024-11-11 14:39:08 +0100

    version.php

--- version.php
+++ version.php
@@ -18,6 +18,7 @@
  * Version details
  *
  * @package   local_chunkupload
+ * @copyright 2024 Thomas Niedermaier <thomas.niedermaier@uni-muenster.de>
  * @copyright 2020 Justus Dieckmann WWU
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -25,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_chunkupload';
-$plugin->release = 'v4.4-r1';
-$plugin->version = 2024060400;
-$plugin->requires = 2022112800; // Requires Moodle 4.1+.
+$plugin->release = 'v4.5-r1';
+$plugin->version = 2024111100;
+$plugin->requires = 2024100700; // Requires Moodle 4.5+.
 $plugin->maturity = MATURITY_STABLE;
